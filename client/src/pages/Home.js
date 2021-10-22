5a8c421f70c599e80d830e22ec6d8b4fc7362869
yap8 <nebozhenko.arseny@gmail.com>
2021-10-22 21:38:58 +0300

    added mui styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,37 @@
-import {
-  Link
-} from 'react-router-dom'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to quiz game</h1>
-      <ul>
-        <li>
-          <Link to="/quizzes">Select a quiz</Link>
-        </li>
-        <li>
-          <Link to="/quizzes/create">Create a quiz</Link>
-        </li>
-      </ul>
-    </div>
+    <Container sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <Typography
+        variant="h2"
+        component="h1"
+        align="center"
+        sx={{ mb: '32px' }}
+      >
+        Welcome to quiz game
+      </Typography>
+      <ButtonGroup>
+        <Button
+          component={Link}
+          to="/quizzes"
+          variant="contained"
+        >
+          Choose a quiz
+        </Button>
+        <Button
+          component={Link}
+          to="/quizzes/create"
+          variant="contained"
+        >
+          Create a quiz
+        </Button>
+      </ButtonGroup>
+    </Container>
   )
 }
 
